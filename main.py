c853a51c14f0b50439743a811ee13f22948b6333
FerNikoMF <76095334+FerNikoMF@users.noreply.github.com>
2024-03-17 21:16:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import ctypes
 from PyQt6.QtWidgets import QApplication
 from font_gui import FontInstallerUI
 
-
 def run_as_admin():
     if sys.platform.startswith('win'):
         try:
-            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-        except:
-            print("Failed to run the script as administrator.")
+            if ctypes.windll.shell32.IsUserAnAdmin() == 0:
+                ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+                sys.exit()  # Выходим, чтобы предотвратить запуск обычного экземпляра программы
+        except Exception as e:
+            print("Failed to run the script as administrator:", e)
 
 if __name__ == "__main__":
-    run_as_admin()
+    run_as_admin()  # Проверяем права администратора
+    
     app = QApplication(sys.argv)
     window = FontInstallerUI()
     
